17ab51daeef3c3744b79d17a345470aede8ae120
nikkovalkow <nikkovalkow@yandex.ru>
2019-07-25 22:03:12 +0300

    Delete get_bitfinex.py

--- get_bitfinex.py
+++
@@ -1,6 +0,0 @@
-try:
-    myclient = pymongo.MongoClient("mongodb://localhost:27017/")
-    mydb = myclient["kufar"]
-    mycol_dead = mydb["data_dead"]
-    mycol=mydb["data"]
-    mycol_sold=mydb["data_sold"]
